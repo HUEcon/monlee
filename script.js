bdf1f7bbb361450dfdb52961bef17a8efa84fd50
HUEcon <ninasxue@gmail.com>
2021-07-22 14:16:42 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
 console.log("Website loaded")
 let isLoading = false;
 
-document.getElementById("submitID").addEventListener("click", function (event) {
-    event.preventDefault()
-    setButtonInactive("submitID")
+var spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1_s9Na_MSCP0uxol-skw4BBsQQ0h-VOBCl852m8ribTw/export?format=csv';
 
-    let participantID = document.getElementById("participantID").value.trim();
-    console.log(participantID);
 
-    if (participantID.length !== 7) {
-        displayError();
-        setButtonActive("submitID")
+function callArray(participantID) {
 
-        return;
-    }
-
-    callLambda(participantID);
-});
-
-function callLambda(participantID) {
-    var r = new XMLHttpRequest();
-    r.open("GET", "https://i4yrsyy66h.execute-api.eu-west-1.amazonaws.com/dev/" + participantID, true);
+     var r = new XMLHttpRequest();
+    r.open("GET", spreadsheetUrl, true);
     document.getElementById("resultsContainer").innerHTML = "";
     r.onreadystatechange = function () {
         if (r.readyState != 4) {
@@ -32,24 +19,89 @@ function callLambda(participantID) {
             setButtonActive("submitID");
         }
 
-        result = JSON.parse(r.responseText);
-
-        if (result.voucher == "TODO") {
+        var result = r.responseText;
+        result = result.split("\n").slice(1)
+        var resultDict = {}
+        result.forEach(x => {
+            var r = x.split(",")
+            resultDict[r[0]] = {
+                "other_transfer": r[1],
+                "final_payoff": r[2],
+                "status": r[3],
+            }
+        })
+        var participantData = resultDict[participantID];
+        if (participantData.status == "todo") {
             displayNotYet();
             setButtonActive("submitID");
             return;
         }
 
-        console.log(result);
-        displayDonation(result);
-        setButtonActive("submitID")
-
-        if (result.voucher !== "NONE") {
-            displayVoucher(result);
+        if (participantData.status !== "todo") {
+            displayOtherTransfer(resultDict[participantID]);
+            displayFinalPayoff(resultDict[participantID]);
+            setButtonActive("submitID");
+            return;
         }
+
     };
+
     r.send();
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+
+document.getElementById("submitID").addEventListener("click", function (event) {
+    event.preventDefault()
+    setButtonInactive("submitID")
+
+    let participantID = document.getElementById("participantID").value.trim();
+    console.log(participantID);
+
+    if (participantID.length !== 8) {
+        displayError();
+        setButtonActive("submitID")
+
+        return;
+    }
+
+    callArray(participantID);
+});
+
+//function callLambda(participantID) {
+//    var r = new XMLHttpRequest();
+//    r.open("GET", "https://i4yrsyy66h.execute-api.eu-west-1.amazonaws.com/dev/" + participantID, true);
+//    document.getElementById("resultsContainer").innerHTML = "";
+//    r.onreadystatechange = function () {
+//        if (r.readyState != 4) {
+//            return;
+//        };
+//
+//        if (r.status != 200) {
+//            displayError();
+//            setButtonActive("submitID");
+//        }
+//
+//        result = JSON.parse(r.responseText);
+//
+//        if (result.status == "todo") {
+//            displayNotYet();
+//            setButtonActive("submitID");
+//            return;
+//        }
+//
+//        console.log(result);
+//        displayDonation(result);
+//        setButtonActive("submitID")
+//
+//        if (result.status !== "ready") {
+//            displayOtherTransfer(result);
+//            displayFinalPayoff(result);
+//        }
+//    };
+//    r.send();
+//}
+
+
 
 function addRow(contentString) {
     let doc = document.getElementById("resultsContainer");
@@ -65,34 +117,31 @@ function addRow(contentString) {
     doc.appendChild(row);
 }
 
-function displayDonation(result) {
+function displayOtherTransfer(result) {
     let resultContent = `    
-        <b>Session time: </b> ${result.session_time} <br>
-        <b>Session ID: </b> ${result.session_id} <br>
-        <b>Donated amount:</b> ${result.donated_amount}€ to charity ${result.charity}<br>
-        <b>Full list of donations:</b> <a href=https://goo.gl/B6j84d target=_blank">https://goo.gl/B6j84d</a> <br>
+        <b>The group you were matched with tranferred: </b> ${result.other_transfer}% <br>
     `
     addRow(resultContent);
 }
 
-function displayVoucher(result) {
+function displayFinalPayoff(result) {
     let resultContent = `      
-        <b>Your Amazon.de gift voucher code:</b> ${result.voucher}<br>
+        <b>Your final payoff:</b> ${result.final_payoff} AUD<br>
+        Please allow a couple of working days for the transfer to be made via PayID <br>
     `
     addRow(resultContent);
 }
 
-function displayError() {
+function displayError(result) {
     let resultContent = `      
-        There was an error retrieving your results. Please make sure you entered the correct code. <br>
-        The code consists of 2 letters and 5 numbers, for example ab12345
+        There was an error retrieving your results. Please make sure you have entered the correct code.<br>
     `
     addRow(resultContent);  
 }
 
-function displayNotYet() {
+function displayNotYet(result) {
     let resultContent = `      
-        Your results are not yet available, please try again in a couple of days.
+        Your results are not yet available, please try again in a couple of days.<br>
     `
     addRow(resultContent);  
 }
